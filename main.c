1cde7a56219a1fefd1f29df3bade46d38586b79e
Matthijs Besselink <matthijsbesselink@live.nl>
2021-05-20 20:23:58 +0200

    got a working openocd based upload config setup.

--- main.c
+++ main.c
@@ -2,17 +2,9 @@
 #define STM32L0
 #endif
 
-#include <errno.h>
-#include <stdio.h>
-#include <stdlib.h>
-
-#include <libopencm3/stm32/rcc.h>
-#include <libopencm3/stm32/usart.h>
-#include <libopencm3/stm32/i2c.h>
-#include <libopencm3/stm32/gpio.h>
-
 int main(void)
 {
     volatile int number = 5;
-    number++;
+    for(int i = 0; i < 10; i++)
+        number++;
 }
--- makefile
+++ makefile
@@ -3,4 +3,12 @@ SRC_DIR = ./src
 BIN_DIR = ./bin
 DEVICE= stm32l041g6
 
+OOCD		= $(shell which openocd)
+
+OOCD_FILE_DIR=/usr/share/openocd/scripts
+OOCD_INTERFACE=stlink-v2
+OOCD_TARGET=stm32l0
+
+
+
 include Makefile.include
