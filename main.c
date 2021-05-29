4bdd48eeea28761ea215433764da2995627f28d1
Matthijs Besselink <matthijsbesselink@live.nl>
2021-05-30 00:12:37 +0200

    current config breaks out of the innermost while. in the i2c write transfer.

--- main.c
+++ main.c
@@ -24,18 +24,36 @@
 
 /* 16MHz * pll_mult / pll_div / hpre / ppreX */
 const struct rcc_clock_scale rcc_hsi_configs[] = {
-	{ /* 16MHz * 3 / 2 / 1 / 1 = 24 */
+	{ /* 24 MHz -> 16 * 3 / 2 = 24 */
     	.pll_source       = RCC_CFGR_PLLSRC_HSI16_CLK,
 		.pll_mul          = RCC_CFGR_PLLMUL_MUL3,
         .pll_div          = RCC_CFGR_PLLDIV_DIV2,
+
+        .flash_waitstates = 2,
+
+        .voltage_scale    = PWR_SCALE1,
+
+		.hpre   = RCC_CFGR_HPRE_NODIV,
+		.ppre1  = RCC_CFGR_PPRE1_NODIV,  // 12 MHz for ppre1
+		.ppre2  = RCC_CFGR_PPRE2_NODIV,  // 12
+
+		.ahb_frequency	= 24000000,
+		.apb1_frequency = 24000000,
+		.apb2_frequency = 24000000,
+        .msi_range  = RCC_ICSCR_MSIRANGE_2MHZ
+	},
+    { /* 12 MHz -> 16 * 3 / 2 = 24 */
+    	.pll_source       = RCC_CFGR_PLLSRC_HSI16_CLK,
+		.pll_mul          = RCC_CFGR_PLLMUL_MUL3,
+        .pll_div          = RCC_CFGR_PLLDIV_DIV4,
 
         .flash_waitstates = 1,
 
         .voltage_scale    = PWR_SCALE1,
 
 		.hpre   = RCC_CFGR_HPRE_NODIV,
-		.ppre1  = RCC_CFGR_PPRE1_NODIV,
-		.ppre2  = RCC_CFGR_PPRE2_NODIV,
+		.ppre1  = RCC_CFGR_PPRE1_NODIV,  // 12 MHz for ppre1
+		.ppre2  = RCC_CFGR_PPRE2_NODIV,  // 12
 
 		.ahb_frequency	= 24000000,
 		.apb1_frequency = 24000000,
@@ -58,14 +76,16 @@ int main(void)
     
     rcc_periph_clock_enable(RCC_GPIOA);
 	gpio_mode_setup(LIS3_CS_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_PULLUP, LIS3_CS_PIN | LIS3_PWR_PIN );
-    
+
+
+
     i2c_link_t i2c = i2c_link_init();
     
     uint8_t wai_data = 0;
     i2c.read(LIS3DH_DEV_ADDR, LIS3DH_WAI_REG, &wai_data, 1);
     printf("wai_data: %d\n", wai_data);
 
-
+    
     // GPIO port B is the port on which the CS pin is.
     rcc_periph_clock_enable(RCC_GPIOB);
 	gpio_mode_setup( DWM_CS_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, DWM_CS_PIN );
--- src/i2c_opencm3.c
+++ src/i2c_opencm3.c
@@ -36,15 +36,13 @@ i2c_link_t i2c_link_init(void)
 {
 	rcc_periph_clock_enable(RCC_I2C1);
 	rcc_periph_clock_enable(RCC_GPIOA);
+	rcc_set_peripheral_clk_sel(I2C1, RCC_CCIPR_I2C1SEL_HSI16);
 
 	// Accelerate the MSI clock (the only one that is active in the uC's reset state.)
     i2c_reset(I2C1);
 
-	rcc_set_peripheral_clk_sel(I2C1_BASE, RCC_CCIPR_I2C1SEL_HSI16);
-	
-
 	// Setup GPIO pin 6 and 7 on GPIO port B for alternate function. 
-	gpio_mode_setup(I2C_PORT, GPIO_MODE_AF, GPIO_PUPD_PULLUP, I2C_PINS);
+	gpio_mode_setup(I2C_PORT, GPIO_MODE_AF, GPIO_PUPD_NONE, I2C_PINS);
     // Set alternate function to i2c.
     // In page 55 of datasheet. I2C1-SCL and SDA.
 	gpio_set_af(I2C_PORT, I2C_AF_ID, I2C_PINS);
@@ -53,12 +51,11 @@ i2c_link_t i2c_link_init(void)
 	i2c_enable_analog_filter(I2C1);
 	i2c_set_digital_filter(I2C1, 0);
 
-	// APB is currently based off of MSI. Which is 2MHz by default.
 	uint32_t clk = rcc_get_i2c_clk_freq(I2C1);
 	
 	printf("clk: %ld", clk );
 
-	i2c_set_speed(I2C1, i2c_speed_fm_400k, clk / 1e6);
+	i2c_set_speed(I2C1, i2c_speed_fm_400k, (clk / 1000000) * 2 );
 	//configure No-Stretch CR1 (only relevant in slave mode)
 	i2c_enable_stretching(I2C1);
 	//addressing mode
